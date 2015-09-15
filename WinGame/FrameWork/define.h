2952739f417bebe910c82d5463f5bec544647f23
The Vinh Luu <luuthevinh7@gmail.com>
2015-09-15 20:46:35 +0700

    update define.h

--- WinGame/FrameWork/define.h
+++ WinGame/FrameWork/define.h
@@ -1,4 +1,4 @@
-#ifndef __NS_FRAMEWORK__
+ï»¿#ifndef __NS_FRAMEWORK__
 #define __NS_FRAMEWORK__
 
 #include <d3d9.h>		// d3d9.lib
@@ -13,8 +13,8 @@ using namespace std;
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
-#define C_WHITE D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f)				// màu tr?ng
-#define COLOR_KEY D3DXCOLOR(1.0f, 0.0f, 1.0f, 1.0f)				// màu khi mà load hình nó b? qua > trong su?t
+#define C_WHITE D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f)				// mÃ u tráº¯ng
+#define COLOR_KEY D3DXCOLOR(1.0f, 0.0f, 1.0f, 1.0f)				// mÃ u khi mÃ  load hÃ¬nh nÃ³ bá» qua > trong suá»‘t
 
 enum eID
 {
