a65f63ccff411b38f31df0c441a2eeb7057e40f9
ryanaleksander <ryan.alva.aleksander@gmail.com>
2015-11-21 07:53:05 -0800

    Commit to pull

--- WinGame/objects/PlayScene.cpp
+++ WinGame/objects/PlayScene.cpp
@@ -72,7 +72,7 @@ bool PlayScene::init()
 
 	auto soldier = new Soldier();
 	soldier->init();
-	soldier->setStatus(eStatus::DYING);
+	soldier->setStatus(eStatus::JUMPING);
 	_listobject.push_back(soldier);
 
 	_text = new Text(L"Arial", "", 10, 25);
