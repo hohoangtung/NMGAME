1e3fac9d00fdf3a2815d70c3e6b0d7bcc97aa769
ryanaleksander <ryan.alva.aleksander@gmail.com>
2015-11-24 21:30:21 -0800

    Commit for pulling

--- WinGame/FrameWork/define.h
+++ WinGame/FrameWork/define.h
@@ -114,9 +114,9 @@ enum eAirCraftType
 
 enum eBulletType
 {
-	BILL_BULLET		= 0,
-	ENEMY_BULLET	= (1 << 0),
-	NORMAL_BULLET   = (1 << 1),
+	BILL_BULLET		= 1,
+	ENEMY_BULLET	= (1 << 1),
+	NORMAL_BULLET   = (1 << 2),
 };
 
 typedef D3DXVECTOR3 GVector3;
